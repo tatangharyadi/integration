bd349a915483c47d375070ee620512996a3dde68
tatangharyadi <tatang.haryadi@outlook.com>
2024-08-26 15:00:33 +0700

    refactor: get customer

--- loyalty/pkg/voucherify/customers.go
+++ loyalty/pkg/voucherify/customers.go
@@ -1,12 +1,56 @@
 package voucherify
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
+
+type VoucherifyMetadata struct {
+	CardGuid                      string `json:"card_guiid"`
+	EmployeeRedemptionDate        string `json:"employee_redemption_date"`
+	EmployeeRedemptionPeriod      string `json:"employee_redemption_period"`
+	EmployeeRedemptionMaxPeriod   int    `json:"employee_redemption_max_period"`
+	EmployeeRedemptionTotalPeriod int    `json:"employee_redemption_total_period"`
+}
+
+type VoucherifyCustomer struct {
+	SourceId string             `json:"source_id"`
+	Name     string             `json:"name"`
+	Phone    string             `json:"phone_number"`
+	Metadata VoucherifyMetadata `json:"metadata"`
+}
+
+type Credit struct {
+	LastTransactionDate string `json:"last_transaction_date"`
+	Period              string `json:"period"`
+	Limit               int    `json:"limit"`
+	CurrentTotal        int    `json:"current_total"`
+}
+
+type Customer struct {
+	CustomerId string `json:"customer_id"`
+	Name       string `json:"name"`
+	Phone      string `json:"phone"`
+	Credit     Credit `json:"credit"`
+}
+
+func MapCustomer(customer VoucherifyCustomer) Customer {
+	return Customer{
+		CustomerId: customer.SourceId,
+		Name:       customer.Name,
+		Phone:      customer.Phone,
+		Credit: Credit{
+			LastTransactionDate: customer.Metadata.EmployeeRedemptionDate,
+			Period:              customer.Metadata.EmployeeRedemptionPeriod,
+			Limit:               customer.Metadata.EmployeeRedemptionMaxPeriod,
+			CurrentTotal:        customer.Metadata.EmployeeRedemptionTotalPeriod,
+		},
+	}
+}
 
 func (rs VoucherifyResource) GetCustomer(w http.ResponseWriter, r *http.Request) {
 	customerId := chi.URLParam(r, "customerId")
@@ -34,6 +78,19 @@ func (rs VoucherifyResource) GetCustomer(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	var VoucherifyCustomer VoucherifyCustomer
+	if err := json.Unmarshal(body, &VoucherifyCustomer); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	customer := MapCustomer(VoucherifyCustomer)
+	resJson, err := json.Marshal(customer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(body)
+	w.Write(resJson)
 }
