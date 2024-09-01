0d895e4d8db5f46fb8898370609d25d42c0ea768
tatangharyadi <tatang.haryadi@outlook.com>
2024-09-01 10:01:21 +0700

    refactor: cleanup

--- loyalty/main.go
+++ loyalty/main.go
@@ -15,7 +15,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World from loyalty"))
+		w.Write([]byte("Hello World from integration-loyalty"))
 	})
 
 	h := voucherify.Handler{
