fc40abc877219c8d549d9c8e2d7763e329cc2e9c
tatangharyadi <tatang.haryadi@outlook.com>
2024-09-01 10:44:30 +0700

    feat: payment

---
+++ payment/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/tatangharyadi/integration/payment/common/configs"
+)
+
+func main() {
+	env, logger := configs.InitEnv()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World from integration-payment"))
+	})
+
+	logger.Info().Msgf("Listening %s mode:%s", env.AppEnv, env.AppPort)
+	addr := ":" + env.AppPort
+	http.ListenAndServe(addr, r)
+}
