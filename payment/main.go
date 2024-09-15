9a721d494f3c413740f1c5a22fc02c29a5805413
tatangharyadi <tatang.haryadi@outlook.com>
2024-09-15 16:28:22 +0700

    refactor: payment

--- payment/main.go
+++ payment/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/tatangharyadi/integration/payment/common/configs"
+	"github.com/tatangharyadi/integration/payment/pkg/xendit"
 )
 
 func main() {
@@ -16,6 +17,12 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World from integration-payment"))
 	})
+
+	h := xendit.Handler{
+		Env:    env,
+		Logger: logger,
+	}
+	r.Mount("/xendit", h.Routes())
 
 	logger.Info().Msgf("Listening %s mode:%s", env.AppEnv, env.AppPort)
 	addr := ":" + env.AppPort
