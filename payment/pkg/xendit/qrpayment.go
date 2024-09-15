673ead85411ccc2359e196b21f99b6e77e2d73d2
tatangharyadi <tatang.haryadi@outlook.com>
2024-09-15 19:32:30 +0700

    refactor: qr payment

--- payment/pkg/xendit/qrpayment.go
+++ payment/pkg/xendit/qrpayment.go
@@ -1,16 +1,97 @@
 package xendit
 
 import (
+	"encoding/json"
 	"net/http"
 
 	pubsub "github.com/tatangharyadi/pos-common/common/pubsub"
 	message "github.com/tatangharyadi/pos-common/protobuf/message"
 )
 
-func (h Handler) CallbackQrPayment(w http.ResponseWriter, r *http.Request) {
-	var qrPayment message.QrPayment
-	qrPayment.Token = "diZjYadBT0KKj1YRZQwQp7:APA91bEnmxuYzvCdvx8uqIr0AlFbHfhPOBAQzdvVmi3JUfzzGaKeo56JIiyMTKmYtJOfx-2KiGNKM0OhBAjrwusFzGlWb7QhDwOGrKGOdgA3n5zycjjUzAeojqamw8NYxwzWp0NUHOyE"
+type XenditPaymentDetail struct {
+	ReceiptId      string `json:"receipt_id"`
+	Source         string `json:"source"`
+	Name           string `json:"name"`
+	AccountDetails string `json:"account_details"`
+}
 
+type XenditMetadata struct {
+	Token string `json:"token"`
+}
+
+type XenditBasket struct {
+	ReferenceId string  `json:"reference_id"`
+	Name        string  `json:"name"`
+	Category    string  `json:"category"`
+	Currency    string  `json:"currency"`
+	Price       float64 `json:"price"`
+	Quantity    int     `json:"quantity"`
+	Type        string  `json:"type"`
+	Url         string  `json:"url"`
+	Description string  `json:"description"`
+	SubCategory string  `json:"sub_category"`
+}
+
+type XenditQrPayment struct {
+	Id            string              `json:"id"`
+	BusinessId    string              `json:"business_id"`
+	Currency      string              `json:"currency"`
+	Amount        float64             `json:"amount"`
+	Status        string              `json:"status"`
+	Created       string              `json:"created"`
+	QrId          string              `json:"qr_id"`
+	QrString      string              `json:"qr_string"`
+	ReferenceId   string              `json:"reference_id"`
+	Type          string              `json:"type"`
+	ChannelCode   string              `json:"channel_code"`
+	ExpireAt      string              `json:"expire_at"`
+	Basket        []XenditBasket      `json:"basket"`
+	Metadata      XenditMetadata      `json:"metadata"`
+	PaymentDetail XenditPaymentDetail `json:"payment_detail"`
+}
+
+func MapQRPayment(xenditQrPayment XenditQrPayment) message.QrPayment {
+	var data = message.QrPayment_Data{
+		Id:          xenditQrPayment.Id,
+		BusinessId:  xenditQrPayment.BusinessId,
+		Currency:    xenditQrPayment.Currency,
+		Amount:      float32(xenditQrPayment.Amount),
+		Status:      xenditQrPayment.Status,
+		Created:     xenditQrPayment.Created,
+		QrId:        xenditQrPayment.QrId,
+		QrString:    xenditQrPayment.QrString,
+		ReferenceId: xenditQrPayment.ReferenceId,
+		Type:        xenditQrPayment.Type,
+		ChannelCode: xenditQrPayment.ChannelCode,
+		ExpiresAt:   xenditQrPayment.ExpireAt,
+		PaymentDetail: &message.QrPayment_Data_PaymentDetail{
+			ReceiptId:      xenditQrPayment.PaymentDetail.ReceiptId,
+			Source:         xenditQrPayment.PaymentDetail.Source,
+			Name:           xenditQrPayment.PaymentDetail.Name,
+			AccountDetails: xenditQrPayment.PaymentDetail.AccountDetails,
+		},
+	}
+
+	return message.QrPayment{
+		Token: xenditQrPayment.Metadata.Token,
+		Notification: &message.QrPayment_Notification{
+			Title: "qr-payment",
+			Body:  xenditQrPayment.Status,
+		},
+		Data: &data,
+	}
+}
+
+func (h Handler) CallbackQrPayment(w http.ResponseWriter, r *http.Request) {
+	var xenditQrPayment XenditQrPayment
+	err := json.NewDecoder(r.Body).Decode(&xenditQrPayment)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//	qrPayment.Token = "diZjYadBT0KKj1YRZQwQp7:APA91bEnmxuYzvCdvx8uqIr0AlFbHfhPOBAQzdvVmi3JUfzzGaKeo56JIiyMTKmYtJOfx-2KiGNKM0OhBAjrwusFzGlWb7QhDwOGrKGOdgA3n5zycjjUzAeojqamw8NYxwzWp0NUHOyE"
+	qrPayment := MapQRPayment(xenditQrPayment)
 	if err := pubsub.PublishProtoMessages(h.Env.GCPProjectId, h.Env.QrPaymentTopic, &qrPayment); err != nil {
 		h.Logger.Error().Err(err).Msg("Error publish message to Pub/Sub")
 		w.WriteHeader(http.StatusInternalServerError)
