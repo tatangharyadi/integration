bf7f1b1385732d5b250822b50c2a07e46e21e993
tatangharyadi <tatang.haryadi@outlook.com>
2024-09-17 09:10:53 +0700

    refactor: qr payment

--- payment/models/payment.go
+++ payment/models/payment.go
@@ -1,11 +1,15 @@
 package models
 
+type PaymentMetadata struct {
+	Token string `json:"token"`
+}
+
 type QrPayment struct {
-	Token       string  `json:"token"`
-	ReferenceId string  `json:"reference_id"`
-	Type        string  `json:"type"`
-	Currency    string  `json:"currency"`
-	Amount      float64 `json:"amount"`
-	QrString    string  `json:"qr_string"`
-	Status      string  `json:"status"`
+	ReferenceId string          `json:"reference_id"`
+	Type        string          `json:"type"`
+	Currency    string          `json:"currency"`
+	Amount      float64         `json:"amount"`
+	QrString    string          `json:"qr_string"`
+	Status      string          `json:"status"`
+	Metadata    PaymentMetadata `json:"metadata"`
 }
